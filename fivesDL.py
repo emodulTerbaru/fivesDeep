1d78c277e91a4e9cf1898ae701718381e98dd2af
emodulTerbaru <emodulfives@gmail.com>
2025-09-22 21:23:44 +0700

    Update fivesDL.py

--- fivesDL.py
+++ fivesDL.py
@@ -12,7 +12,7 @@ def tampilkan1():
     <iframe src='https://martin123-oke.github.io/fives/kover_depan.html' style='width:100%; height:600px;'></iframe>
     """,unsafe_allow_html=True)
 def tampilkan2():
-    tab = st.tabs(["Visual","Fitur Interaktif"])
+    tab = st.tabs(["Visual","Fitur Interaktif","Aktivitas"])
     with tab[0]:
         st.markdown("""
         <iframe src='https://martin123-oke.github.io/fives/cerita_fakta.html' style='width:100%; height:2500px;'></iframe>
@@ -21,13 +21,30 @@ def tampilkan2():
         st.markdown("""
         <iframe src='https://martin123-oke.github.io/fives/Fitur_interaktif.html' style='width:100%; height:2500px;'></iframe>
         """,unsafe_allow_html=True)
+    with tab[2]:
+        st.markdown("""
+        <iframe src='https://martin123-oke.github.io/fives/Aktivitas.html' style='width:100%; height:2500px;'></iframe>
+        """,unsafe_allow_html=True)
 
-
+def tampilkan3():
+    tab = st.tabs(["Visual","Fitur Interaktif","Aktivitas"])
+    with tab[0]:
+        st.markdown("""
+        <iframe src='https://martin123-oke.github.io/fives/visual2.html' style='width:100%; height:2500px;'></iframe>
+        """,unsafe_allow_html=True)
+    with tab[1]:
+        st.markdown("""
+        <iframe src='https://martin123-oke.github.io/fives/Fitur2.html' style='width:100%; height:3700px;'></iframe>
+        """,unsafe_allow_html=True)
+    with tab[2]:
+        st.write("Akan dilanjutkan")
 
 if st.session_state.kumpulan['kondisi1']:
     tampilkan1()
 if st.session_state.kumpulan['kondisi2']:
     tampilkan2()
+if st.session_state.kumpulan['kondisi3']:
+    tampilkan3()
     
 if st.sidebar.button('Tampilan Awal'):
     st.session_state.kumpulan={'kondisi1':True, 'kondisi2':False, 'kondisi3':False,
@@ -38,7 +55,9 @@ if st.sidebar.button('Fakta (F)'):
                                'kondisi4':False, 'kondisi5':False, 'kondisi6':False, 'kondisi7':False}
     st.rerun()
 if st.sidebar.button('Inferensi (I)'):
-    pass
+    st.session_state.kumpulan={'kondisi1':False, 'kondisi2':False, 'kondisi3':True,
+                               'kondisi4':False, 'kondisi5':False, 'kondisi6':False, 'kondisi7':False}
+    st.rerun()
 if st.sidebar.button('Kosakata (V)'):
     pass
 if st.sidebar.button('Pengalaman (E)'):
